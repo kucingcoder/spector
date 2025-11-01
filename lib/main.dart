40670b099ad8d5ff816593b5416e3c87d6fb1513
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2025-11-01 20:19:18 +0700

    feat: update AndroidManifest and permissions, enhance webview functionality, and add file picker support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_refresher/webview_refresher.dart';
 
 void main() {
@@ -31,8 +34,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final controller = WebViewController();
+  late final WebViewController controller;
   Completer<void>? _completer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        WebViewController(onPermissionRequest: (request) => request.grant())
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onPageFinished: (_) => finishRefresh(),
+              onWebResourceError: (_) => finishRefresh(),
+            ),
+          )
+          ..loadRequest(Uri.parse('https://garasiasatu.com/mobile'));
+
+    if (Platform.isAndroid) {
+      final AndroidWebViewController androidController =
+          controller.platform as AndroidWebViewController;
+      androidController.setOnShowFileSelector(_androidFilePicker);
+    }
+  }
+
+  Future<List<String>> _androidFilePicker(FileSelectorParams params) async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.any,
+        allowMultiple: false,
+        withData: false,
+      );
+
+      if (result == null || result.files.isEmpty) return [];
+
+      final filePath = result.files.single.path;
+      if (filePath == null) return [];
+
+      final fileUri = Uri.file(filePath).toString();
+      debugPrint('Picked file URI: $fileUri');
+
+      return [fileUri];
+    } catch (e) {
+      debugPrint('File picker error: $e');
+      return [];
+    }
+  }
 
   Future<void> onRefresh() {
     _completer = Completer<void>();
@@ -47,27 +95,15 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (url) => finishRefresh(),
-          onWebResourceError: (error) => finishRefresh(),
-        ),
-      )
-      ..loadRequest(Uri.parse('https://garasiasatu.com/mobile'));
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(0),
-        child: AppBar(backgroundColor: Colors.white24),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: WebviewRefresher(controller: controller, onRefresh: onRefresh),
+        ),
       ),
-      body: WebviewRefresher(controller: controller, onRefresh: onRefresh),
     );
   }
 }
